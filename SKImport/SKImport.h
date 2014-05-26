d31728720768ce518ce356c24e8524d12788c005
Adrian Cooney <cooney.adrian@gmail.com>
2014-05-26 15:56:39 +0100

    Initial Commit

---
+++ SKImport/SKImport.h
@@ -0,0 +1,13 @@
+//
+//  SKImport.h
+//  SKImport
+//
+//  Created by Adrian Cooney on 26/05/2014.
+//  Copyright (c) 2014 Adrian Cooney. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SKImport : NSObject
+
+@end
---
+++ SKImport/SKImport.m
@@ -0,0 +1,13 @@
+//
+//  SKImport.m
+//  SKImport
+//
+//  Created by Adrian Cooney on 26/05/2014.
+//  Copyright (c) 2014 Adrian Cooney. All rights reserved.
+//
+
+#import "SKImport.h"
+
+@implementation SKImport
+
+@end
